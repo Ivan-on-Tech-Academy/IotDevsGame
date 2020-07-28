3b7637d00e1885bab91ad01617692f0055c8c9c3
dani69654 <martinelli.daniele7@gmail.com>
2020-07-28 16:19:42 +0200

    bugs fixed

--- contracts/Game/Easy/Level_0.sol
+++ contracts/Game/Easy/Level_0.sol
@@ -8,26 +8,22 @@ interface playerContract {
 * @dev Player has to create a contract that includes a function setData(),
 *      this function will receive a randomly generated address.
 *      The player has to send any amount of ether to that address.
-* @notice setData () should return true.
 */
 
 contract Entrance {
 
+  struct playerStruct {
+    address  targetAddress;
+  }
+  mapping (address => playerStruct) public player;
   playerContract usr = playerContract (0x0);
 
+
   function ent_0 (address _playerContractAddress) public returns (bool){
-
-    address targetAddress;
-    bool check;
-
     setInterface(_playerContractAddress);
-
-    targetAddress = randomAddress();
-
-    check = usr.setData(targetAddress);
-
-    require (check && address(targetAddress).balance > 0,"Target balance should be > than 0");
-
+    player[msg.sender].targetAddress = randomAddress();
+    usr.setData(player[msg.sender].targetAddress);
+    require (address(player[msg.sender].targetAddress).balance > 0,"Target balance should be > than 0");
     return true;
   }
 
@@ -38,5 +34,4 @@ contract Entrance {
   function randomAddress () private view returns (address){
       return address(uint160(uint256(now)));
     }
-
 }
