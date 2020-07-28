132c968381a72e7cba8a6f1dd5296fd5f29ce808
dani69654 <martinelli.daniele7@gmail.com>
2020-07-28 12:24:58 +0200

    general modifications

--- contracts/Game/Easy/Entrance.sol
+++ contracts/Game/Easy/Entrance.sol
@@ -1,43 +1,41 @@
 pragma solidity 0.6.2;
 
 interface playerContract {
-    function setData(address) external;
+    function setData(address) external returns (bool);
 }
 
 /**
-* @dev Player have to set a contract that includes a function setData(),
+* @dev Player has to create a contract that includes a function setData(),
 *      this function will receive a randomly generated address.
 *      The player has to send any amount of ether to that address.
+* @notice setData () should return true.
 */
 
 contract Entrance {
-
+  
   playerContract usr = playerContract (0x0);
 
   function ent_0 (address _playerContractAddress) public returns (bool){
 
     address targetAddress;
+    bool check;
 
     setInterface(_playerContractAddress);
 
-    (targetAddress) = antiHack();
+    targetAddress = randomAddress();
 
-    usr.setData(targetAddress);
+    check = usr.setData(targetAddress);
 
-    require (address(targetAddress).balance > 0,"Target balance should be > than 0");
+    require (check && address(targetAddress).balance > 0,"Target balance should be > than 0");
 
     return true;
-
   }
 
   function setInterface (address _playerContractAddress) internal {
       usr = playerContract(_playerContractAddress);
-
   }
 
-  function antiHack () private view returns (address){
-
-    //@dev address(uint160(uint256(now))) >> generates a random address
+  function randomAddress () private view returns (address){
       return address(uint160(uint256(now)));
     }
 
