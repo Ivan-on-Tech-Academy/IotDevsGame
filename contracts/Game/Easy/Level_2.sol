0567693073eabee43ad474fa96894165b375c4e9
dani69654 <martinelli.daniele7@gmail.com>
2020-07-28 14:28:47 +0200

    .

---
+++ contracts/Game/Easy/Level_2.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.6.2;
+
+/**
+* @dev Shoot for the moon. Even if you miss, you'll land among the stars.
+*/
+
+contract ThinkBig {
+
+    uint256 a = 2;
+
+    function go (uint256 _n) internal returns (bool) {
+        require (_n > 0 && _n*a < a, "failed");
+        return true;
+    }
+
+}
