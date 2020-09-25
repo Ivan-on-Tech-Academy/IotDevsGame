61b58832171cabfb7341f0ff0031d1347ddbd873
dani69654 <martinelli.daniele7@gmail.com>
2020-09-25 18:42:13 +0200

    added 721 token tests

--- contracts/Game_Controls/Players.sol
+++ contracts/Game_Controls/Players.sol
@@ -1,36 +1,62 @@
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity 0.6.2;
 
-contract Players {
+contract Players is ERC721 {
 
   using SafeMath for uint256;
-  using SafeMath for uint16;
+
+  uint256 private tokenCounter;
+  address [] public playersList;
+
+  constructor ()
+  ERC721("PlayerToken","PLT")
+  public {
+    tokenCounter = 0;
+    _safeMint(address(this),tokenCounter);
+    tokenCounter = tokenCounter.add(1);
+  }
 
   struct Player {
-    uint256 playerLevel;
-    bool isPlayer;
+    uint256 tokenId; // A unique ERC721 generated for the player
+    uint256 playerLevel; // The level of the player
     mapping(address => address) instanceByLvl; // save the instance address for _lvl address
     mapping(address => bool) completeLevels; // if lvl is complete = true
   }
 
   mapping (address => Player) public players;
 
-  /**
-  * @dev Enable a new player.
-  * @notice Registration cost is 1 ether.
-  */
-  function addPlayer () public payable {
-    require (msg.value == 1 ether,"Register a new player costs 1 ether");
-    players[msg.sender].isPlayer = true;
+  modifier isPlayer () {
+    require (players[msg.sender].tokenId != 0,'not a player');
+    _;
   }
 
-  /**
-  * @dev A player can disable itself.
-  */
-  function removePlayer () public   {
-    delete(players[msg.sender]);
+  function registerNewPlayer () public {
+    _createNewToken (msg.sender);
   }
 
+  function _createNewToken (address _player) private {
+    require (players[_player].tokenId == 0,'player has already a token');
+    players[_player].tokenId = tokenCounter;
+    tokenCounter = tokenCounter.add(1);
+    playersList.push(_player);
+    _safeMint(_player,players[_player].tokenId);
+  }
+
+  function _increasePlayerLevel (address _player) internal {
+    require(ownerOf(players[_player].tokenId) == _player);
+    players[_player].playerLevel = players[_player].playerLevel.add(1);
+  }
+
+  function getPlayerLevel (address _player) public view returns (uint256) {
+    return players[_player].playerLevel;
+  }
+
+  // testing
+
+  function mock () public {
+    _increasePlayerLevel(msg.sender);
+  }
 
 }
